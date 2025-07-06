fe3c7fa0579519dda65ec1f1de1c6f922d084fed
Sandya Vutla <vutlasandhya84@gmail.com>
2025-07-06 15:36:14 +0530

    Add Week 3 Zip via Git LFS

---
+++ Week3Assignments/Configuring a Basic Spring Application/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBook() {
+        String book = bookRepository.getBook();
+        System.out.println("Book Name: " + book);
+    }
+}
